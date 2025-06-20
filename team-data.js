67c6cab2dd5231ec10c0365ac15a2ea2120458a4
Ganesh4502 <ganesh115784@gmail.com>
2025-06-20 13:44:31 +0530

    Add files via upload

---
+++ team-data.js
@@ -0,0 +1,44 @@
+// team-data.js
+
+const teamData = {
+  2025: [
+    { name: "Anil Kumar", rank: "SUO", image: "images/anil.jpg" },
+    { name: "Sneha Reddy", rank: "JUO", image: "images/yoshitha.jpg" },
+    { name: "Suresh Babu", rank: "SGT", image: "images/bhavana.jpg" },
+    { name: "Lakshmi Devi", rank: "CPL", image: "images/durga.jpg" },
+    { name: "Manoj Kumar", rank: "SGT", image: "images/durga.jpg" },
+    { name: "Divya Patel", rank: "LCPL", image: "images/durga.jpg" }
+  ],
+  2024: [
+    { name: "Anil Kumar", rank: "SUO", image: "images/anil.jpg" },
+    { name: "Sneha Reddy", rank: "JUO", image: "images/yoshitha.jpg" },
+    { name: "Suresh Babu", rank: "SGT", image: "images/bhavana.jpg" },
+    { name: "Lakshmi Devi", rank: "CPL", image: "images/durga.jpg" },
+    { name: "Manoj Kumar", rank: "SGT", image: "images/durga.jpg" },
+    { name: "Divya Patel", rank: "LCPL", image: "images/durga.jpg" }
+  ],
+  2023: [
+    { name: "Anil Kumar", rank: "SUO", image: "images/anil.jpg" },
+    { name: "Sneha Reddy", rank: "JUO", image: "images/yoshitha.jpg" },
+    { name: "Suresh Babu", rank: "SGT", image: "images/bhavana.jpg" },
+    { name: "Lakshmi Devi", rank: "CPL", image: "images/durga.jpg" },
+    { name: "Manoj Kumar", rank: "SGT", image: "images/durga.jpg" },
+    { name: "Divya Patel", rank: "LCPL", image: "images/durga.jpg" }
+  ],
+ 2022: [
+    { name: "Anil Kumar", rank: "SUO", image: "images/anil.jpg" },
+    { name: "Sneha Reddy", rank: "JUO", image: "images/yoshitha.jpg" },
+    { name: "Suresh Babu", rank: "SGT", image: "images/bhavana.jpg" },
+    { name: "Lakshmi Devi", rank: "CPL", image: "images/durga.jpg" },
+    { name: "Manoj Kumar", rank: "SGT", image: "images/durga.jpg" },
+    { name: "Divya Patel", rank: "LCPL", image: "images/durga.jpg" }
+  ],
+ 2021: [
+    { name: "Anil Kumar", rank: "SUO", image: "images/anil.jpg" },
+    { name: "Sneha Reddy", rank: "JUO", image: "images/yoshitha.jpg" },
+    { name: "Suresh Babu", rank: "SGT", image: "images/bhavana.jpg" },
+    { name: "Lakshmi Devi", rank: "CPL", image: "images/durga.jpg" },
+    { name: "Manoj Kumar", rank: "SGT", image: "images/durga.jpg" },
+    { name: "Divya Patel", rank: "LCPL", image: "images/durga.jpg" }
+  ]
+};
